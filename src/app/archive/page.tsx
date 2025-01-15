8f0b5236da2dffc2b09b91d086639d4b44510cfa
sykim0181 <soyeon364@naver.com>
2025-01-15 15:55:16 +0900

    FIX: archive 페이지 초기 설정 수정

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -10,6 +10,7 @@ import { useTypedSelector } from '@/lib/redux/store';
 import { setAlbumToPlay } from '@/lib/redux/playerInfo';
 import { setAlbumList } from '@/lib/redux/archivedAlbumList';
 import MainLayout from '@/layouts/MainLayout';
+import { setIsLpOnTurntable, setSelectedAlbum } from '@/lib/redux/selectedAlbum';
 
 const InteractiveArchive = dynamic(
   () => import('@/components/common/InteractiveArchive'),
@@ -31,6 +32,8 @@ function Page() {
     const storedAlbum = getAlbumToPlayFromSessionStorage();
     if (storedAlbum) {
       dispatch(setAlbumToPlay(storedAlbum));
+      dispatch(setSelectedAlbum({ album: storedAlbum }));
+      dispatch(setIsLpOnTurntable(true));
     }
     dispatch(setAlbumList(getAlbumListFromSessionStorage()))
   }, [dispatch]);
