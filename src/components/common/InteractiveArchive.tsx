faf18190f470abd745cd9ee11e11fe81f0169f6e
sykim0181 <soyeon364@naver.com>
2025-02-23 18:21:09 +0900

    FIX: InteractiveArchive 훅 분리
    - 비즈니스 로직 hook으로 분리

--- src/components/common/InteractiveArchive.tsx
+++ src/components/common/InteractiveArchive.tsx
@@ -1,19 +1,18 @@
 'use client'
 
-import React, { useEffect, useMemo, useRef } from "react";
+import { useMemo } from "react";
 import { useDispatch } from "react-redux";
 import { RxPlus } from "react-icons/rx";
 
 import styles from '@/styles/common/InteractiveArchive.module.scss';
-import { useTypedSelector } from "@/lib/redux/store";
 import { Album } from "@/types/type";
 import LpComponent from "./LpComponent";
 import LpVinyl from "../LpVinyl";
 import BottomArea from "../Archive/BottomArea";
-import { clearSelectedAlbum, moveVinyl, setIsLpOnTurntable, setVinylPosition } from "@/lib/redux/selectedAlbum";
 import FloatingButtons from "../Archive/FloatingButtons";
 import { setModal } from "@/lib/redux/modalInfo";
 import { LIMIT_NUM_ALBUM } from "@/constants";
+import useInteractiveArchive from "@/hooks/useInteractiveArchive";
 
 interface InteractiveArchiveProps {
   albumList: Album[];
@@ -23,158 +22,20 @@ interface InteractiveArchiveProps {
 const InteractiveArchive = (props: InteractiveArchiveProps) => {
   const { albumList, isEditable } = props;
 
-  const selectedAlbum = useTypedSelector(state => state.selectedAlbum.album);
-  const selectedLpPosition = useTypedSelector(state => state.selectedAlbum.pos);
-  const lpIsOnTurntable = useTypedSelector(state => state.selectedAlbum.isOnTurntable);
-  
-  const floatingVinylRef = useRef<HTMLDivElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
-
   const dispatch = useDispatch();
 
-  const isMovingVinyl = !lpIsOnTurntable && selectedAlbum;
-
-  useEffect(() => {
-    const elements = document.getElementsByClassName(styles.list_lp);
-    const lpListElement = elements[0] as HTMLDivElement;
-
-    if (isMovingVinyl) {
-      lpListElement.style.overflowY ='hidden';
-    } else {
-      lpListElement.style.overflowY ='scroll';
-    }
-  }, [isMovingVinyl]);
-
-  const onMouseMove = (e: React.MouseEvent) => {
-    if (lpIsOnTurntable) {
-      return;
-    }
-
-    if (selectedAlbum !== null) {
-      if (selectedLpPosition.x === null || selectedLpPosition.y === null) {
-        return;
-      }
-
-      const currentPosX = selectedLpPosition.x + e.movementX;
-      const currentPosY = selectedLpPosition.y + e.movementY;
-      const lpSize = getFloatingVinylSize() ?? 120;
-
-      const lpPlatterElement = document.getElementById('lp-platter');
-      const boundingRect = lpPlatterElement?.getBoundingClientRect();
-      if (boundingRect === undefined) {
-        return;
-      }
-      const lpPlatterLeft = boundingRect.left;
-      const lpPlatterRight = boundingRect.right;
-      const lpPlatterTop = boundingRect.top;
-      const lpPlatterBottom = boundingRect.bottom;
-      if (
-        boundingRect &&
-        (currentPosX >= lpPlatterLeft && currentPosX + lpSize <= lpPlatterRight) &&
-        (currentPosY >= lpPlatterTop && currentPosY + lpSize <= lpPlatterBottom)
-      ) {
-        // 턴테이블에 lp 올려놓기
-        dispatch(setIsLpOnTurntable(true));
-      } else {
-        dispatch(moveVinyl({
-          deltaX: e.movementX,
-          deltaY: e.movementY,
-        }));
-        dispatch(setIsLpOnTurntable(false));
-      }
-    }
-  }
-
-  const onMouseUp = () => {
-    if (isMovingVinyl) {
-      dispatch(clearSelectedAlbum());
-    }
-  }
-
-  const onTouchMove = (e: React.TouchEvent) => {
-    if (!isMovingVinyl) {
-      return;
-    }
-    e.preventDefault();
-
-    if (selectedLpPosition.x === null || selectedLpPosition.y === null) {
-      return;
-    }
-
-    if (floatingVinylRef.current) {
-      const lpBoundingRect = floatingVinylRef.current.getBoundingClientRect();
-      const lpWidth = lpBoundingRect.width;
-      const lpHeight = lpBoundingRect.height;
-      
-      const currentPosX = e.touches[0].clientX - lpWidth / 2;
-      const currentPosY = e.touches[0].clientY - lpWidth / 2;
-  
-      const lpPlatterElement = document.getElementById('lp-platter');
-      const platterBoundingRect = lpPlatterElement?.getBoundingClientRect();
-      if (platterBoundingRect === undefined) {
-        return;
-      }
-      const lpPlatterLeft = platterBoundingRect.left;
-      const lpPlatterRight = platterBoundingRect.right;
-      const lpPlatterTop = platterBoundingRect.top;
-      const lpPlatterBottom = platterBoundingRect.bottom;
-      if (
-        platterBoundingRect &&
-        (currentPosX >= lpPlatterLeft && currentPosX + lpWidth <= lpPlatterRight) &&
-        (currentPosY >= lpPlatterTop && currentPosY + lpHeight <= lpPlatterBottom)
-      ) {
-        // 턴테이블에 lp 올려놓기
-        dispatch(setIsLpOnTurntable(true));
-      } else {
-        dispatch(setVinylPosition({
-          posX: currentPosX,
-          posY: currentPosY
-        }));
-        dispatch(setIsLpOnTurntable(false));
-      }
-    }
-  }
-
-  const onTouchEnd = () => {
-    if (isMovingVinyl) {
-      dispatch(clearSelectedAlbum());
-    }
-  }
-
-  const showFloatingVinyl = 
-    isMovingVinyl && 
-    selectedLpPosition.x !== null &&
-    selectedLpPosition.y !== null
-  ;
-
-  const getFloatingVinylSize = () => {
-    const listItem = document.getElementsByClassName(styles.list_lp_item)?.[0];
-    if (listItem !== undefined) {
-      const boundingRect = listItem.getBoundingClientRect();
-      const width = boundingRect.width;
-      return width;
-    }
-  }
-  const floatingVinylSize = getFloatingVinylSize();
-
-  const getFloatingVinylPosition = () => {
-    const pos: { x: number | undefined; y: number | undefined } = {
-      x: undefined,
-      y: undefined
-    };
-    if (
-      containerRef.current &&
-      selectedLpPosition.x !== null &&
-      selectedLpPosition.y !== null
-    ) {
-      const containerRect = containerRef.current.getBoundingClientRect();
-      const padding = 10;
-      pos.x = selectedLpPosition.x - containerRect.x + padding;
-      pos.y = selectedLpPosition.y - containerRect.y + padding;
-    }
-    return pos;
-  }
-  const floatingVinylPosition = getFloatingVinylPosition();
+  const { 
+    floatingVinylRef,
+    containerRef,
+    handleMouseMove,
+    handleMouseUp, 
+    handleTouchMove,
+    handleTouchEnd,
+    showFloatingVinyl,
+    floatingVinylPosition,
+    floatingVinylSize,
+    selectedAlbum
+  } = useInteractiveArchive();
 
   const AlbumList = useMemo(() => {
     const onClickAddSongBtn = () => {
@@ -210,10 +71,10 @@ const InteractiveArchive = (props: InteractiveArchiveProps) => {
     <div
       className={styles.container}
       ref={containerRef}
-      onMouseMove={onMouseMove}
-      onMouseUp={onMouseUp}
-      onTouchMove={onTouchMove}
-      onTouchEnd={onTouchEnd}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
     >
       <div className={styles.content}>
         {AlbumList}
@@ -238,7 +99,7 @@ const InteractiveArchive = (props: InteractiveArchiveProps) => {
         }}
         onContextMenu={() => { return false }}
       >
-        <LpVinyl album={selectedAlbum} />
+        <LpVinyl album={selectedAlbum!} />
       </div> 
       )}
     </div>
