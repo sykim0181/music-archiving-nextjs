7bdcd7252ffd354b5221827a49d6cbb79e98b7f3
sykim0181 <soyeon364@naver.com>
2025-09-02 20:33:09 +0900

    refactor: tanstack query client 관리 방식 변경
    - 서버는 항상 새로운 query client 사용
    - 클라이언트는 처음에만 새로 만들기

--- src/components/QueryClientContextProvider.tsx
+++ src/components/QueryClientContextProvider.tsx
@@ -1,11 +1,36 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactNode } from "react";
 
-const queryClient = new QueryClient();
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
 
 const QueryClientContextProvider = ({ children }: { children: ReactNode }) => {
+  const queryClient = getQueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
