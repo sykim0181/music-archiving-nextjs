5d9c31ddd1f7b290a5ce36afae79d8ba125475c7
sykim0181 <soyeon364@naver.com>
2025-09-04 17:38:45 +0900

    refactor: 로그인 페이지 서버 컴포넌트로 전환

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,44 +1,15 @@
-"use client";
-
 import "./page.scss";
 import "@/styles/commonStyle.scss";
-import { createClient } from "@/utils/supabase/client";
 import MainLayout from "@/layouts/MainLayout";
+import LoginButtons from "@/components/Login/LoginButtons";
 
 const LoginPage = () => {
-  const supabase = createClient();
-
-  const onClickKakaoButton = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: "kakao",
-      options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}`,
-      },
-    });
-  };
-
-  const onClickSpotifyButton = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: "spotify",
-      options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/login/spotify/callback`,
-      },
-    });
-  };
-
   return (
     <MainLayout>
       <div className="login_container">
         <h1>로그인</h1>
 
-        <div className="button_group">
-          <button className="kakao_button" onClick={onClickKakaoButton}>
-            Kakao로 로그인
-          </button>
-          <button className="spotify_button" onClick={onClickSpotifyButton}>
-            Spotify로 로그인
-          </button>
-        </div>
+        <LoginButtons />
       </div>
     </MainLayout>
   );
---
+++ src/components/Login/LoginButtons.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { createClient } from "@/utils/supabase/client";
+
+const LoginButtons = () => {
+  const supabase = createClient();
+
+  const onClickKakaoButton = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "kakao",
+      options: {
+        redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      },
+    });
+  };
+
+  const onClickSpotifyButton = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "spotify",
+      options: {
+        redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/login/spotify/callback`,
+      },
+    });
+  };
+
+  return (
+    <div className="button_group">
+      <button className="kakao_button" onClick={onClickKakaoButton}>
+        Kakao로 로그인
+      </button>
+      <button className="spotify_button" onClick={onClickSpotifyButton}>
+        Spotify로 로그인
+      </button>
+    </div>
+  );
+};
+
+export default LoginButtons;
