2f23e502f3a6c3707619b42da65367c9f04ad902
sykim0181 <soyeon364@naver.com>
2025-09-06 00:05:23 +0900

    fix: 유저 토큰 관리 방식 변경에 따른 로그아웃 동작 수정

--- src/components/Header/LogInOutButton.tsx
+++ src/components/Header/LogInOutButton.tsx
@@ -1,32 +1,21 @@
 "use client";
 
 import useUser from "@/hooks/useUser";
-import { setAccessToken } from "@/lib/redux/spotify";
 import { createClient } from "@/utils/supabase/client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useDispatch } from "react-redux";
 
 const LogInoutButton = () => {
   const user = useUser();
 
-  const dispatch = useDispatch();
   const router = useRouter();
 
   const onClickLogoutButton = async () => {
     const supabase = createClient();
     const { error } = await supabase.auth.signOut();
     if (error) {
-      console.log("Failed to sign out:", error);
+      alert("로그아웃 실패");
     } else {
-      const response = await fetch(`/api/auth/logout`, {
-        method: "POST",
-      });
-      const data = await response.json();
-      console.log("/api/auth/logout", data);
-      if (data.token) {
-        dispatch(setAccessToken(data.token));
-      }
       alert("로그아웃 완료");
       router.push("/");
     }
