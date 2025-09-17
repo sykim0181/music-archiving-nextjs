ae37af9ff1cb8f64e2fe904669a8788e7a932045
sykim0181 <soyeon364@naver.com>
2025-09-17 15:30:06 +0900

    style: 플레이어 디자인 수정
    - 너비 560px 기준으로 mini/max 사이즈 플레이어 배치 수정

--- src/components/common/Player/AlbumPlayerContents.tsx
+++ src/components/common/Player/AlbumPlayerContents.tsx
@@ -18,6 +18,7 @@ import {
 } from "@/lib/redux/playerThunk";
 import { useState } from "react";
 import { PlayerSize } from "./Player";
+import { useMediaQuery } from "react-responsive";
 
 interface Props {
   album: AlbumWithTrack;
@@ -27,8 +28,9 @@ interface Props {
 const AlbumPlayerContents = ({ album, playerSize }: Props) => {
   const [hasPlayed, setHasPlayed] = useState(false);
   const isPlaying = useTypedSelector((state) => state.player.isPlaying);
-
   const currentTrack = useTypedSelector((state) => state.player.track);
+
+  const isPhabletDisplay = useMediaQuery({ query: "(max-width: 560px)" });
 
   const dispatch = useAppDispatch();
 
@@ -82,117 +84,170 @@ const AlbumPlayerContents = ({ album, playerSize }: Props) => {
   }
 
   if (playerSize === "MINIMISED") {
-    return (
-      <div className="mini-sized-player album-player-container bg-gradient">
-        <div className="left-part">
-          <div className="album-cover-container">
-            <Image src={album.imageUrl} alt="" fill />
-          </div>
-          <div className="album-info">
-            <p className="current-track-name">
-              {album.tracks.find((value) => value.id === currentTrack?.id)
-                ?.name ?? ""}
-            </p>
-            <p>{`${album.name} · ${album.artists.join(", ")}`}</p>
-          </div>
-        </div>
+    const currentTrackName =
+      album.tracks.find((value) => value.id === currentTrack?.id)?.name ?? "";
+    const albumCover = (
+      <div className="album-cover-container">
+        <Image src={album.imageUrl} alt="" fill />
+      </div>
+    );
+    const prevButton = (
+      <button
+        className="icon_button previous-button"
+        onClick={onClickPrevButton}
+      >
+        <IoIosSkipBackward />
+      </button>
+    );
+    const playPauseButton = isPlaying ? (
+      <button
+        className="icon_button play-button bg_black"
+        onClick={onClickPauseButton}
+      >
+        <IoIosPause />
+      </button>
+    ) : (
+      <button
+        className="icon_button play-button bg_black"
+        onClick={onClickPlayButton}
+      >
+        <IoIosPlay />
+      </button>
+    );
+    const nextButton = (
+      <button className="icon_button next-button" onClick={onClickNextButton}>
+        <IoIosSkipForward />
+      </button>
+    );
 
-        <div className="right-part">
-          <div className="button-group">
-            <button
-              className="icon_button previous-button"
-              onClick={onClickPrevButton}
-            >
-              <IoIosSkipBackward />
-            </button>
-            {isPlaying ? (
-              <button
-                className="icon_button play-button bg_black"
-                onClick={onClickPauseButton}
-              >
-                <IoIosPause />
-              </button>
-            ) : (
-              <button
-                className="icon_button play-button bg_black"
-                onClick={onClickPlayButton}
-              >
-                <IoIosPlay />
-              </button>
-            )}
-            <button
-              className="icon_button next-button"
-              onClick={onClickNextButton}
-            >
-              <IoIosSkipForward />
-            </button>
-          </div>
-        </div>
+    return (
+      <div
+        className={`mini-sized-player ${
+          isPhabletDisplay
+            ? "mini-sized-player--small"
+            : "mini-sized-player--large"
+        } album-player-container bg-gradient`}
+      >
+        {isPhabletDisplay ? (
+          <>
+            <div className="left-part">{albumCover}</div>
+            <div className="right-part">
+              <div className="album-info">
+                <p className="current-track-name">{currentTrackName}</p>
+              </div>
+              <div className="button-group">
+                {prevButton}
+                {playPauseButton}
+                {nextButton}
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="left-part">
+              {albumCover}
+              <div className="album-info">
+                <p className="current-track-name">{currentTrackName}</p>
+                <p>{`${album.name} · ${album.artists.join(", ")}`}</p>
+              </div>
+            </div>
+
+            <div className="right-part">
+              <div className="button-group">
+                {prevButton}
+                {playPauseButton}
+                {nextButton}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     );
   }
 
   // playerSize === "MAXiMISED"
-  return (
-    <div className="max-sized-player album-player-container bg-gradient">
-      <div className="left_content">
-        <div className="album_cover_container">
-          <Image src={album.imageUrl} alt="" fill />
-        </div>
-        <div className="button_group">
-          <button
-            className="icon_button prev_button"
-            onClick={onClickPrevButton}
+  const trackList = (
+    <div className="track_list_container">
+      <ul className="track-list invisible_scroll">
+        {album.tracks.map((track, idx) => (
+          <li
+            key={track.id}
+            className={`track-item ${
+              track.id === currentTrack?.id ? "track-item--selected" : ""
+            }`}
+            onClick={() => onClickTrackItem(idx)}
           >
-            <RxTrackPrevious />
-          </button>
-          {isPlaying ? (
-            <button
-              className="icon_button play_button"
-              onClick={onClickPauseButton}
-            >
-              <RxPause />
-            </button>
-          ) : (
-            <button
-              className="icon_button play_button"
-              onClick={onClickPlayButton}
-            >
-              <RxPlay />
-            </button>
-          )}
-          <button
-            className="icon_button next_button"
-            onClick={onClickNextButton}
-          >
-            <RxTrackNext />
-          </button>
-        </div>
-      </div>
+            <div className="track-item-index">{idx + 1}</div>
+            <div>{track.name}</div>
+            <div>{msToString(track.duration)}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+  const albumCover = (
+    <div className="album_cover_container">
+      <Image src={album.imageUrl} alt="" fill />
+    </div>
+  );
+  const playerButtons = (
+    <div className="button_group">
+      <button className="icon_button prev_button" onClick={onClickPrevButton}>
+        <RxTrackPrevious />
+      </button>
+      {isPlaying ? (
+        <button
+          className="icon_button play_button"
+          onClick={onClickPauseButton}
+        >
+          <RxPause />
+        </button>
+      ) : (
+        <button className="icon_button play_button" onClick={onClickPlayButton}>
+          <RxPlay />
+        </button>
+      )}
+      <button className="icon_button next_button" onClick={onClickNextButton}>
+        <RxTrackNext />
+      </button>
+    </div>
+  );
+  const albumInfo = (
+    <div className="album_info">
+      <p className="album_name">{album.name}</p>
+      <p className="album_artist">{`- ${album.artists.join(", ")}`}</p>
+    </div>
+  );
 
-      <div className="right_content">
-        <div className="album_info">
-          <p className="album_name">{album.name}</p>
-          <p className="album_artist">{`- ${album.artists.join(", ")}`}</p>
+  return (
+    <div
+      className={`max-sized-player ${
+        isPhabletDisplay ? "max-sized-player--small" : "max-sized-player--large"
+      } album-player-container bg-gradient`}
+    >
+      {isPhabletDisplay ? (
+        <>
+          <div className="upper-content">
+            <div className="upper-left-content">{albumCover}</div>
+            <div className="upper-right-content">
+              {albumInfo}
+              {playerButtons}
+            </div>
+          </div>
+          <div className="lower-content">{trackList}</div>
+        </>
+      ) : (
+        <div className="upper-content">
+          <div className="upper-left-content">
+            {albumCover}
+            {playerButtons}
+          </div>
+          <div className="upper-right-content">
+            {albumInfo}
+            {trackList}
+          </div>
         </div>
-        <div className="track_list_container">
-          <ul className="track-list invisible_scroll">
-            {album.tracks.map((track, idx) => (
-              <li
-                key={track.id}
-                className={`track-item ${
-                  track.id === currentTrack?.id ? "track-item--selected" : ""
-                }`}
-                onClick={() => onClickTrackItem(idx)}
-              >
-                <div className="track-item-index">{idx + 1}</div>
-                <div>{track.name}</div>
-                <div>{msToString(track.duration)}</div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
