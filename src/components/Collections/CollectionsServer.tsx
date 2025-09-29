4aa6cab083217cba17bca6ab84c3a7bee61b6cfa
sykim0181 <soyeon364@naver.com>
2025-09-29 14:36:25 +0900

    chore: prefetch

--- src/components/Collections/CollectionsServer.tsx
+++ src/components/Collections/CollectionsServer.tsx
@@ -1,8 +1,15 @@
 import { Category } from "@/app/collections/page";
 import CollectionsClient from "./CollectionsClient";
 import { createClient } from "@/utils/supabase/server";
-import { getPublicCollections } from "@/lib/supabase/fetchForCommon";
-import { getCollectionItems } from "@/utils/collectionUtils";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import {
+  getUsePublicCollectionItemsQueryOptions,
+  getUseUserCollectionItemsQueryOptions,
+} from "@/hooks/useCollectionItemsQuery/queryOptions";
 
 interface CollectionsContentContainerProps {
   limit: number;
@@ -14,16 +21,29 @@ const CollectionsServer = async ({
   category,
 }: CollectionsContentContainerProps) => {
   const supabase = await createClient();
-  const publicCollections = await getPublicCollections(supabase, 0, limit - 1);
-  const collectionItems = await getCollectionItems(publicCollections, true);
+  const queryClient = new QueryClient();
+
+  async function getQueryOptions() {
+    if (category === "user") {
+      const { data } = await supabase.auth.getUser();
+      return getUseUserCollectionItemsQueryOptions(
+        supabase,
+        data.user?.id,
+        limit,
+        true
+      );
+    }
+    return getUsePublicCollectionItemsQueryOptions(supabase, limit, true);
+  }
+
+  const queryOptions = await getQueryOptions();
+  await queryClient.prefetchInfiniteQuery(queryOptions);
+  const state = dehydrate(queryClient);
 
   return (
-    <CollectionsClient
-      key={category}
-      category={category}
-      limit={limit}
-      initialData={collectionItems}
-    />
+    <HydrationBoundary state={state}>
+      <CollectionsClient key={category} category={category} limit={limit} />
+    </HydrationBoundary>
   );
 };
 
